bbf08f4e31205607ed55a4fa7d290232c4ac238f
brunolralves <brunolralves@yahoo.com>
2022-10-25 09:59:36 -0300

    Implementadas partes faltantes do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new(numero: "991047435",modelo: "N3380",imei: "418547215848544846454",memoria: 64);
+Iphone iphone = new("991000902","Pro MAX","454545412158422454",128);
+
+
+System.Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("Smartphone Iphone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
